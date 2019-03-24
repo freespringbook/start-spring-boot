6c03c413efd3d28603634b7b8f37d0224a90636b
freelife1191 <freelife1191.good@gmail.com>
2019-03-24 21:50:22 +0900

    로그인 정보 설정하기 - 최신버전에서 실패

--- src/main/java/me/freelife/security/SecurityConfig.java
+++ src/main/java/me/freelife/security/SecurityConfig.java
@@ -1,6 +1,8 @@
 package me.freelife.security;
 
 import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,6 +10,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Log
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        log.info("build Auth global...........");
+
+        auth.inMemoryAuthentication()
+                .withUser("manager")
+                .password("1111")
+                .roles("MANAGER");
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         log.info("security config.....................");
@@ -22,4 +35,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.formLogin();
     }
+
+
 }
